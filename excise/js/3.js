396a3fd5ebc2974fb9c66cf18f13fef0d5a8fe94
yangcan <492573329@qq.com>
2018-10-12 16:59:12 +0800

    更新HTML5

---
+++ excise/js/3.js
@@ -0,0 +1,35 @@
+function hello() {
+    if (!Modernizr.canvas) {
+        alert("不支持cavas!!");
+    }
+
+    if (!Modernizr.video) {
+        alert("不支持video!!");
+    }
+     //本地存储
+    if (!Modernizr.localstorage) {
+
+        alert("不支持localStorage!");
+    }
+    //Web Worker
+    if(!Modernizr.webworkers){
+
+    	alert("不支持webWork!")
+    }
+    //本地
+    if(!Modernizr.applicationcache) {
+    	alert("不支持applicationcache!")
+    }
+       //地理位置 geolocation
+    if(!Modernizr.geolocation) {
+    	alert("不支持地理位置!")
+    }
+
+
+}
+
+
+/*function suports_local_storage() {
+    return ('localStorage' in window)
+
+}*/
