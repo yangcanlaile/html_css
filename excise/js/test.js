de4e1f280163d4c944d9edd7efbe4866b70a9e90
yangcan <492573329@qq.com>
2018-10-29 15:42:12 +0800

    commit

---
+++ excise/js/test.js
@@ -0,0 +1,8 @@
+$(function(){
+
+	console.log("124")
+	$('table#languages td:nth-child(1)').css("color", "blue");
+	$('table#languages td:nth-child(2)').css("color", "red");
+	$('table#languages td:nth-child(3)').css("color", "yellow");
+})
+
