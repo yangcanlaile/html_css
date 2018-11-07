78e091bf3ec5f78a11777fb3b3fa43764cb3f0d3
Administrator <492573329@qq.com>
2018-11-08 01:09:07 +0800

    通用提交

--- python/pratice.py
+++ python/pratice.py
@@ -1,31 +1,54 @@
-# -*- coding:UTF-8 -*-
-from bs4 import BeautifulSoup
 import requests
-import os
+from bs4 import BeautifulSoup
+import sys
 
 
-class ClassName(object):
-    """docstring for ClassName"""
+class downloader(object):
+    """下载器"""
 
-   def __init__(self):
-        self.server = 'http://www.biqukan.com/'
-        self.target = 'http://www.biqukan.com/1_1094/'
-        self.names = []  # 存放章节名
-        self.urls = []  # 存放章节链接
-        self.nums = 0  # 章节数
-
-    """
-    函数说明:获取下载链接
-    Parameters:
-        无
-    Returns:
-        无
-    Modify:
-        2017-09-13
-    """
+    def __init__(self):
+        self.server = "https://www.biqukan.com"
+        self.target = "https://www.biqukan.com/1_1094/"
+        self.names = []
+        self.urls = []
+        self.nums = 0
 
     def get_download_url(self):
         req = requests.get(url=self.target)
         html = req.text
-        div_bf = BeautifulSoup(html)
-        d
+        div_bf = BeautifulSoup(html, features="html.parser")
+        div = div_bf.find_all('div', class_='listmain')
+        a_bf = BeautifulSoup(str(div[0]), features="html.parser")
+        a = a_bf.find_all('a')
+        aAll = a[15:25]
+        self.nums = len(aAll)
+        for each in aAll:
+            self.names.append(each.string)
+            self.urls.append(self.server + each.get('href'))
+
+    def get_content(self, target):
+        req = requests.get(target)
+        html = req.text
+        div_bf = BeautifulSoup(html, features="html.parser")
+        div = div_bf.find_all(id='content', class_='showtxt')
+        content = div[0].text.replace('\xa0' * 8, '\n\n')
+        return content
+
+    def write(self, path, name, content):
+        with open(path, 'a', encoding='utf-8') as f:
+            f.write(name + '\n')
+            f.writelines(content)
+            f.write('\n\n')
+
+
+if __name__ == "__main__":
+    dl = downloader()
+    # div = dl.get_download_url()
+    dl.get_download_url()
+    print("开始下载小说..........")
+    for i in range(dl.nums):
+        content = dl.get_content(dl.urls[i])
+        dl.write("1.txt", dl.names[i], content)
+        sys.stdout.write("下载: \r{0:.2f}".format(
+            ((i + 1) / dl.nums) * 100) + "%")
+    print("下载完成")
