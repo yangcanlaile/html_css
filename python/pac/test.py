a1b40d1a92aaa02e83bd74776b7364149d14d465
yangcanlaile <13710097312@163.com>
2019-02-11 18:35:32 +0800

    通用提交

--- python/pac/test.py
+++ python/pac/test.py
@@ -1,7 +1,3 @@
-from math import sqrt
-from sys import stdout
-for m in range(101, 201):
-    k = int(sqrt(m + 1))
-    for i in range(2, k + 1):
-        if m % i == 0:
-            leap = 0
+from bs4 import BeautifulSoup
+soup = BeautifulSoup('<p>Hello</p>', 'lxml')
+print(soup.p.string)
